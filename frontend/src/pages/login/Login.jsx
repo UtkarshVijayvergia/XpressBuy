a303d642e3aa1dc05a84f6de9c580065cff408c4
Utkarsh <utkarsh.v1901@gmail.com>
2024-01-30 12:24:23 +0530

    integrate AWS Amplify login authentication

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,60 +1,78 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-// import { login, reset } from '../../features/auth/authSlice'
 import { Link } from 'react-router-dom'
 // import { toast } from 'react-toastify'
 import { FaLock, FaEnvelope } from 'react-icons/fa'
-// import Spinner from '../../components/Spinner'
 import './login.css'
+
+// AWS Amplify authenication
+import { Auth } from 'aws-amplify';
 
 
 const Login = () => {
+    const navigate = useNavigate()
+    
     const [formData, setFormData] = useState({
-        username: "",
+        email: "",
         password: "",
     })
+    const { email, password } = formData;
 
-    const { username, password } = formData;
-
-    const navigate = useNavigate()
+    // sign in with aws-amplify and aws cognito
+    const [errors, setErrors] = React.useState('');
+    
 
     // useEffect(() => {
     //     if(isError){
     //       toast.error(message)
     //     }
-  
     //     if(isSuccess || user){
     //       navigate('/home')
     //     }
-  
     //     dispatch(reset())
-      
     // }, [user, isError, isSuccess, message, navigate, dispatch])
 
+
     const onChange = (e) => {
-        setFormData( (prevState) => ({
+        setFormData((prevState) => ({
             ...prevState,
             [e.target.name]: e.target.value,
         }))
     }
 
-    const onSubmit = (e) => {
-        e.preventDefault();
 
-        // const userData = {
-        //     username,
-        //     password,
-        // }
-
-        // dispatch(login(userData))
-        console.log(username, password);
-    }
-
-
-    // if(isLoading){
-    //     return <Spinner />
+    // const onSubmit = (e) => {
+    //     e.preventDefault();
+    //     // const userData = {
+    //     //     username,
+    //     //     password,
+    //     // }
+    //     // dispatch(login(userData))
+    //     console.log(username, password);
     // }
+    
+
+    // aws-amplify aws cognito - sign in
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        setErrors('');
+        try {
+            console.log(email, password);
+            const user = await Auth.signIn(email, password);
+            console.log(user);
+            localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken);
+            console.log(localStorage.getItem("access_token"));
+            window.location.href = "/"
+        } catch (error) {
+            if (error.code === 'UserNotConfirmedException') {
+                window.location.href = "/confirm";
+            }
+            setErrors(error.message);
+            console.log('error signing in', error);
+        }
+        return false;
+    }
 
 
     return (
@@ -64,7 +82,7 @@ const Login = () => {
                     <div className='row'>
                         <div className="col">
                             <div className='img-login'>
-                                <img src={require(`../../assets/images/login.jpg`)} height="150px" alt="ShareConnect"/>
+                                <img src={require(`../../assets/images/login.jpg`)} height="150px" alt="ShareConnect" />
                             </div>
                             <div className='toRegister'>
                                 <b><Link className="no-decoration" aria-current="page" to={`/signup`}>Create an Account</Link></b>
@@ -78,14 +96,14 @@ const Login = () => {
                                 <form onSubmit={onSubmit}>
                                     <div className="form-group formPadder">
                                         <div className='inputTaker form-control'>
-                                            <FaEnvelope className='faFig'/>
-                                            <input type="username" id='username' name='username' value={username} placeholder='Username' onChange={onChange}/>
+                                            <FaEnvelope className='faFig' />
+                                            <input type="email" id='email' name='email' value={email} placeholder='Email' onChange={onChange} />
                                         </div>
                                     </div>
                                     <div className="form-group formPadder">
                                         <div className='inputTaker form-control'>
-                                            <FaLock className='faFig'/>
-                                            <input type="password" id='password' name='password' value={password} placeholder='Your Password' onChange={onChange}/>
+                                            <FaLock className='faFig' />
+                                            <input type="password" id='password' name='password' value={password} placeholder='Your Password' onChange={onChange} />
                                         </div>
                                     </div>
                                     <div className="form-group submitForm">
