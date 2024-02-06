50f200f6936aa2228667a4f3f7692e9559597377
Utkarsh <utkarsh.v1901@gmail.com>
2024-02-06 22:36:44 +0530

    Add Postgres database connection module

---
+++ backend/db/postgres/db.js
@@ -0,0 +1,13 @@
+// Initiate connection to the Postgres database
+const { Pool } = require('pg')
+
+const POSTGRES_CONNECTION_URL = process.env.POSTGRES_CONNECTION_URL
+// POSTGRES_CONNECTION_URL="postgres://postgres:password@postgres-db:5432/xpressbuy"
+
+
+
+const pool = new Pool({
+    connectionString: POSTGRES_CONNECTION_URL,
+})
+
+module.exports = pool;
