0f168189dd69c8de76def84dde9bae46ab823004
Utkarsh <utkarsh.v1901@gmail.com>
2024-02-07 19:53:36 +0530

    Connected with AWS RDS

--- backend/db/postgres/db.js
+++ backend/db/postgres/db.js
@@ -1,7 +1,7 @@
 // Initiate connection to the Postgres database
 const { Pool } = require('pg')
 
-const POSTGRES_CONNECTION_URL = process.env.POSTGRES_CONNECTION_URL
+const POSTGRES_CONNECTION_URL = process.env.POSTGRES_PROD_CONNECTION_URL
 // POSTGRES_CONNECTION_URL="postgres://postgres:password@postgres-db:5432/xpressbuy"
 
 
