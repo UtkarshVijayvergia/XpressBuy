c694fa42d54c40654964a35eaf6ad2bd4acbd093
Utkarsh <utkarsh.v1901@gmail.com>
2024-02-25 12:42:44 +0530

    Add category list and fetch categories from API

--- frontend/src/pages/products/Categories.jsx
+++ frontend/src/pages/products/Categories.jsx
@@ -6,7 +6,23 @@ import Navbar from '../../components/navbar/Navbar'
 
 
 const Categories = () => {
+    const [category, setCategory] = useState([])
     const [products, setProducts] = useState([]);
+
+    // Get all categories
+    const getCategories = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/api/v1/products/category', {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                method: 'GET',
+            });
+            setCategory(await response.json());
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     // Get all products
     const getProducts = async () => {
@@ -23,16 +39,71 @@ const Categories = () => {
         }
     }
 
+    // useEffect
     useEffect(() => {
+        getCategories();
         getProducts();
     }, []);
+
 
     return (
         <div>
             <Navbar />
             <div className='cat-product-container'>
                 <div className='left-column'>
+                    <div className="left-column-placement">
+                        <div className="category-left-column-heading">
+                            Categories
+                        </div>
+                        <div className='category-list-placement'>
+                            {
+                                category.map((category, index) => {
+                                    return (
+                                        <div key={index} className='category-list'>
+                                            <div className='category-name'>
+                                                {
+                                                    category.sk[0] >= '0' && category.sk[0] <= '9'
+                                                        ?
+                                                        category.sk[0] == 1
+                                                            ?
+                                                            <div className='category-name category-name-upper'>
+                                                                EVERYTHING
+                                                                <div className="category-quantity">
+                                                                    ({category.product_quantity})
+                                                                </div>
+                                                            </div>
+                                                            :
+                                                            category.sk[0] == 2
+                                                                ?
+                                                                <div className='category-name category-name-upper'>
+                                                                    MEN
+                                                                    <div className="category-quantity">
+                                                                        ({category.product_quantity})
+                                                                    </div>
+                                                                </div>
+                                                                :
+                                                                <div className=' category-name category-name-lower'>
+                                                                    WOMEN
+                                                                    <div className="category-quantity">
+                                                                        ({category.product_quantity})
+                                                                    </div>
+                                                                </div>
+                                                        :
+                                                        <div className='category-name'>
+                                                            {category.sk}
+                                                            <div className="category-quantity">
+                                                                ({category.product_quantity})
+                                                            </div>
+                                                        </div>
+                                                }
 
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
                 </div>
                 <div className='right-column'>
                     {
@@ -42,7 +113,7 @@ const Categories = () => {
                                     <img src={products[index][0].imageURL} alt='product' />
                                     <div className='product-details'>
                                         <div className='product-name'>
-                                            { products[index][0].name }
+                                            {products[index][0].name}
                                         </div>
                                         <div className='product-price'>
                                             $100
