40f7c2f8f8e02fba51248c33d91a60e3ff0638f9
Utkarsh <utkarsh.v1901@gmail.com>
2024-02-26 23:45:35 +0530

    Refactor HalfStar component to improve precision

--- frontend/src/components/star-rating/HalfStar.jsx
+++ frontend/src/components/star-rating/HalfStar.jsx
@@ -4,12 +4,13 @@ import './starRating.css';
 
 const HalfStar = ({ className, color, size, rating }) => {
     const halfs = (Math.round((rating - Math.floor(rating))*100));
+    const precision = halfs<50 ? halfs + (100-halfs)/10 : halfs - (100-halfs)/10;
   
     return (
       <div className="half-star-wrapper">
-                <AiFillStar className={className} color="#e4e5e9" size={size} />
-            <div className="half-star-overlay" style={{ width: `${halfs}%` }}>
-            <AiFillStar className={className} color={color} size={size} />
+            <AiFillStar className={className} color="#e4e5e9" size={size} />
+            <div className="half-star-overlay" style={{ width: `${precision}%` }}>
+                <AiFillStar className={className} color={color} size={size} />
             </div>
         </div>
     )
