27fddcb11e3c1e24167575fd982eeb14005d3cd5
Utkarsh <utkarsh.v1901@gmail.com>
2024-02-27 19:14:49 +0530

    navbar navigation bug fixed

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FaShoppingCart } from 'react-icons/fa'
 import { Auth } from 'aws-amplify'
 import './navbar.css'
 
 
-const Navbar = (props) => {
-    // const [user, setUser] = useState(null);
+const Navbar = ({ setCategoryName }) => {
     const location = useLocation();
-    
+
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
     // function to check if user is authenticated
@@ -18,11 +18,6 @@ const Navbar = (props) => {
             await Auth.currentAuthenticatedUser({ bypassCache: false });
             setIsAuthenticated(true);
             console.log("User is authenticated");
-            // setUser({
-            //     email: user.attributes.email,
-            //     name: user.attributes.name,
-            //     username: user.attributes.preferred_username,
-            // });
         } catch (err) {
             setIsAuthenticated(false);
             console.log(err);
@@ -33,64 +28,69 @@ const Navbar = (props) => {
     useEffect(() => {
         checkUser();
     }, []);
-    // XpressBuy_Logo_Chars_Black_NoBg
 
     return (
         <div>
-            <nav className={ location.pathname=='/' ? 'navbar' : 'navbar-cat' }>
+            <nav className={location.pathname == '/' ? 'navbar' : 'navbar-cat'}>
                 <ul className="navbar-nav">
                     <li className="nav-item-logo">
-                        <a href="/">
+                        <Link to="/">
                             {
-                                location.pathname=='/' ?
-                                <img className='navbar-logo-symbol' src={require(`../../assets/images/XpressBuy/XpressBuy_Logo_Design_NoBg_White.png`)} alt='logo' />
-                                :
-                                <img className='navbar-logo-symbol' src={require(`../../assets/images/XpressBuy/XpressBuy_Logo_Design_NoBg_Black.png`)} alt='logo' />
+                                location.pathname == '/' ?
+                                    <img className='navbar-logo-symbol' src={require(`../../assets/images/XpressBuy/XpressBuy_Logo_Design_NoBg_White.png`)} alt='logo' />
+                                    :
+                                    <img className='navbar-logo-symbol' src={require(`../../assets/images/XpressBuy/XpressBuy_Logo_Design_NoBg_Black.png`)} alt='logo' />
                             }
-                        </a>
+                        </Link>
                     </li>
                     <li className="nav-item-xpressbuy">
-                        <a href="/">
+                        <Link to="/">
                             {
-                                location.pathname=='/' ?
-                                <img className='navbar-logo' src={require(`../../assets/images/XpressBuy/XpressBuy_Logo_Chars_NoBg.png`)} alt='logo' />
-                                :
-                                <img className='navbar-logo' src={require(`../../assets/images/XpressBuy/XpressBuy_Logo_Chars_Black_NoBg.png`)} alt='logo' />
+                                location.pathname == '/' ?
+                                    <img className='navbar-logo' src={require(`../../assets/images/XpressBuy/XpressBuy_Logo_Chars_NoBg.png`)} alt='logo' />
+                                    :
+                                    <img className='navbar-logo' src={require(`../../assets/images/XpressBuy/XpressBuy_Logo_Chars_Black_NoBg.png`)} alt='logo' />
                             }
-                        </a>
+                        </Link>
                     </li>
                     <div className='nav-item-direction'>
                         <div className='nav-item-left'>
                             <li className="nav-item">
-                                <a className={ location.pathname=='/' ? 'nav-item-name' : location.pathname=='/products/018DE5AA-91A7-BD3C-F93C-C1FADA5DC1C4' ? 'nav-item-curr-cat-name' : 'nav-item-cat-name' } href="/products/018DE5AA-91A7-BD3C-F93C-C1FADA5DC1C4">EVERYTHING</a>
+                                <Link className={location.pathname == '/' ? 'nav-item-name' : location.pathname == '/products/018DE5AA-91A7-BD3C-F93C-C1FADA5DC1C4' ? 'nav-item-curr-cat-name' : 'nav-item-cat-name'}
+                                    onClick={() => setCategoryName && setCategoryName('018DE5AA-91A7-BD3C-F93C-C1FADA5DC1C4')}
+                                    to="/products/018DE5AA-91A7-BD3C-F93C-C1FADA5DC1C4">EVERYTHING</Link>
                             </li>
                             <li className="nav-item">
-                                <a className={ location.pathname=='/' ? 'nav-item-name' : location.pathname=='/products/018DE5AC-A4DC-2600-C495-D8E31F87C887' ? 'nav-item-curr-cat-name' : 'nav-item-cat-name' } href="/products/018DE5AC-A4DC-2600-C495-D8E31F87C887">MEN</a>
+                                <Link className={location.pathname == '/' ? 'nav-item-name' : location.pathname == '/products/018DE5AC-A4DC-2600-C495-D8E31F87C887' ? 'nav-item-curr-cat-name' : 'nav-item-cat-name'}
+                                    onClick={() => setCategoryName && setCategoryName('018DE5AC-A4DC-2600-C495-D8E31F87C887')}
+                                    to="/products/018DE5AC-A4DC-2600-C495-D8E31F87C887">MEN</Link>
                             </li>
                             <li className="nav-item">
-                                <a className={ location.pathname=='/' ? 'nav-item-name' : location.pathname=='/products/018DE5AC-BAB0-00C1-0248-2816E5309CCF' ? 'nav-item-curr-cat-name' : 'nav-item-cat-name' } href="/products/018DE5AC-BAB0-00C1-0248-2816E5309CCF">WOMEN</a>
+                                <Link className={location.pathname == '/' ? 'nav-item-name' : location.pathname == '/products/018DE5AC-BAB0-00C1-0248-2816E5309CCF' ? 'nav-item-curr-cat-name' : 'nav-item-cat-name'}
+                                    onClick={() => setCategoryName && setCategoryName('018DE5AC-BAB0-00C1-0248-2816E5309CCF')}
+                                    to="/products/018DE5AC-BAB0-00C1-0248-2816E5309CCF">WOMEN</Link>
                             </li>
                             <li className="nav-item">
-                                <a className={ location.pathname=='/' ? 'nav-item-name' : 'nav-item-cat-name' } href="/">SPECIALS</a>
+                                <Link className={location.pathname == '/' ? 'nav-item-name' : 'nav-item-cat-name'} to="/">SPECIALS</Link>
                             </li>
                         </div>
                         <div className='nav-item-right'>
                             <li className="nav-item">
-                                <a className={ location.pathname=='/' ? 'nav-item-name' : 'nav-item-cat-name' } href="/">ABOUT</a>
+                                <Link className={location.pathname == '/' ? 'nav-item-name' : 'nav-item-cat-name'} to="/">ABOUT</Link>
                             </li>
                             <li className="nav-item">
-                                <a className={ location.pathname=='/' ? 'nav-item-name' : 'nav-item-cat-name' } href="/">CONTACT US</a>
+                                <Link className={location.pathname == '/' ? 'nav-item-name' : 'nav-item-cat-name'} to="/">CONTACT US</Link>
                             </li>
                             {
                                 isAuthenticated ? (
                                     <>
                                         <li className="nav-item">
-                                            <a className={ location.pathname=='/' ? 'nav-item-name' : 'nav-item-cat-name' } href="/"> $0 &nbsp; <FaShoppingCart /></a>
+                                            <Link className={location.pathname == '/' ? 'nav-item-name' : 'nav-item-cat-name'} to="/"> $0 &nbsp; <FaShoppingCart /></Link>
                                         </li>
                                     </>
                                 ) : <>
                                     <li className="nav-item nav-last-item">
-                                        <a className={ location.pathname=='/' ? 'nav-item-name' : 'nav-item-cat-name' } href="/signup">SIGN IN</a>
+                                        <Link className={location.pathname == '/' ? 'nav-item-name' : 'nav-item-cat-name'} to="/signup">SIGN IN</Link>
                                     </li>
                                 </>
                             }
--- frontend/src/pages/products/Categories.jsx
+++ frontend/src/pages/products/Categories.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useParams } from "react-router-dom";
+import { useLocation } from 'react-router-dom'
 import './categories.css'
 import Navbar from '../../components/navbar/Navbar'
 import StarRating from '../../components/star-rating/StarRating'
+import ErrorPage from '../errorPage/ErrorPage'
 
 
 const Categories = () => {
-    const { id } = useParams();
+    const location = useLocation();
     const [category, setCategory] = useState([])
     const [products, setProducts] = useState([]);
-    const [categoryName, setCategoryName] = useState('018DE5AA-91A7-BD3C-F93C-C1FADA5DC1C4');
+    const [categoryName, setCategoryName] = useState('');
 
     // Post category name to get products
     const navigate = useNavigate();
     const handleCategory = (categoryName) => {
         setCategoryName(categoryName);
-        console.log(categoryName);
         navigate(`/products/${categoryName}`);
+        getProducts(categoryName);
     }
 
     // Get all categories
@@ -37,7 +38,7 @@ const Categories = () => {
     }
 
     // Get all products
-    const getProducts = async () => {
+    const getProducts = async (categoryName) => {
         try {
             const response = await fetch(`http://localhost:5000/api/v1/products/${categoryName}`, {
                 headers: {
@@ -54,42 +55,89 @@ const Categories = () => {
     // useEffect
     useEffect(() => {
         getCategories();
-        getProducts();
+        handleCategory(location.pathname.split('/')[2]);
     }, [categoryName]);
 
 
     return (
         <div>
-            <Navbar />
-            <div className='cat-product-container'>
-                <div className='left-column'>
-                    <div className="left-column-placement">
-                        <div className="category-left-column-heading">
-                            Categories
+            {products.length > 0 ?
+                <>
+                    <Navbar setCategoryName={setCategoryName} handleCategory={handleCategory} />
+                    <div className='cat-product-container'>
+                        <div className='left-column'>
+                            <div className="left-column-placement">
+                                <div className="category-left-column-heading">
+                                    Categories
+                                </div>
+                                <div className='category-list-placement'>
+                                    {
+                                        category.map((category, index) => {
+                                            return (
+                                                <div key={index} className='category-list'>
+                                                    <div className='category-name'>
+                                                        {
+                                                            ((index === 0) || (index === 2))
+                                                                ?
+                                                                <div className='category-name category-name-lower' onClick={() => handleCategory(`${category.sk}`)}>
+                                                                    {category.category_name}
+                                                                    <div className="category-quantity">
+                                                                        ({category.product_quantity})
+                                                                    </div>
+                                                                </div>
+                                                                :
+                                                                <div className='category-name' onClick={() => handleCategory(`${category.sk}`)}>
+                                                                    {category.category_name}
+                                                                    <div className="category-quantity">
+                                                                        ({category.product_quantity})
+                                                                    </div>
+                                                                </div>
+                                                        }
+                                                    </div>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
                         </div>
-                        <div className='category-list-placement'>
+                        <div className='right-column'>
                             {
-                                category.map((category, index) => {
+                                products.map((image, index) => {
                                     return (
-                                        <div key={index} className='category-list'>
-                                            <div className='category-name'>
-                                                {
-                                                    ((index === 0) || (index === 2)) 
-                                                    ?
-                                                    <div className='category-name category-name-lower' onClick={() => handleCategory(`${category.sk}`)}>
-                                                        {category.category_name}
-                                                        <div className="category-quantity">
-                                                            ({category.product_quantity})
-                                                        </div>
+                                        <div key={index} className='product-card'>
+                                            <img className='product-image' src={products[index][0].imageURL} alt='product' />
+                                            <div className='product-details'>
+                                                <div className='product-name'>
+                                                    {products[index][0].product_name}
+                                                </div>
+                                                <div className='product-review-rating'>
+                                                    <div className='product-rating-star'>
+                                                        <StarRating rating={products[index][0].product_rating} />
                                                     </div>
-                                                    :
-                                                    <div className='category-name' onClick={() => handleCategory(`${category.sk}`)}>
-                                                        {category.category_name}
-                                                        <div className="category-quantity">
-                                                            ({category.product_quantity})
-                                                        </div>
+                                                    <div className='product-rating'>
+                                                        ({products[index][0].product_rating})
                                                     </div>
-                                                }
+
+                                                    <div className="product-inverse-carrot">
+                                                        <sup>` </sup>
+                                                    </div>
+                                                    <div className="product-reviews">
+                                                        <sup>{products[index][0].product_reviews}</sup>
+                                                    </div>
+
+                                                </div>
+                                                <div className="product-sold">
+                                                    {products[index][0].product_sold}+ bought this product
+                                                </div>
+                                                <div className="product-price-details">
+                                                    <div className="product-price-currency">
+                                                        <sup>Rs.</sup>
+                                                    </div>
+                                                    <div className='product-price'>
+                                                        {products[index][0].product_price}
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     )
@@ -97,54 +145,14 @@ const Categories = () => {
                             }
                         </div>
                     </div>
+                    <div className='page-end'>
+                    </div>
+                </>
+                :
+                <div className='no-products'>
+                    <ErrorPage />
                 </div>
-                <div className='right-column'>
-                    {
-                        products.map((image, index) => {
-                            return (
-                                <div key={index} className='product-card'>
-                                    <img className='product-image' src={products[index][0].imageURL} alt='product' />
-                                    <div className='product-details'>
-                                        <div className='product-name'>
-                                            {products[index][0].product_name}
-                                        </div>
-                                        <div className='product-review-rating'>
-                                            <div className='product-rating-star'>
-                                                <StarRating rating={products[index][0].product_rating} /> 
-                                            </div>
-                                            <div className='product-rating'>
-                                                ({products[index][0].product_rating})
-                                            </div>
-                                            
-                                                <div className="product-inverse-carrot">
-                                                    <sup>` </sup>
-                                                </div>
-                                                <div className="product-reviews">
-                                                <sup>{products[index][0].product_reviews}</sup>
-                                                </div>
-                                            
-                                        </div>
-                                        <div className="product-sold">
-                                            {products[index][0].product_sold}+ bought this product
-                                        </div>
-                                        <div className="product-price-details">
-                                            <div className="product-price-currency">
-                                                <sup>Rs.</sup>
-                                            </div>
-                                            <div className='product-price'>
-                                                {products[index][0].product_price}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-            </div>
-            <div className='page-end'>
-                {/* <hr /> */}
-            </div>
+            }
         </div>
     )
 }
