f0fbcb35d4ec2929afc963a7921062a860ed26f3
Utkarsh <utkarsh.v1901@gmail.com>
2024-02-28 23:42:50 +0530

    Add product details controller and route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { getAllProducts, getAllCategories, getProductsByCategory } = require('../controller/categoryFilterController')
+const { getAllProducts, getProductsByCategory } = require('../controller/categoryFilterController')
+const { getSingleProduct } = require('../controller/productDetailsController')
 
 // routes for /api/v1/category
 router.get('', getAllProducts)
 router.get('/:category_id', getProductsByCategory)
+router.get('/product/:product_id', getSingleProduct)
 
 
 module.exports = router;
