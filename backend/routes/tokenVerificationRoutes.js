792466b967e3092022f898e5c9cc7e6b8139b054
Utkarsh Vijayvergia <utkarsh.v1901@gmail.com>
2024-03-12 17:25:10 -0700

    Refactor login verification routes

---
+++ backend/routes/tokenVerificationRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+const asyncHandler = require('express-async-handler')
+const { loginVerification } = require('../middlewares/userLoginVerificationMiddlewares')
+const { verifyAccessTokenMiddleware } = require('../middlewares/tokenVerificationMiddlewares')
+
+// royes for /api/v1/tokenVerification
+router.post('/verifyIdToken', loginVerification, asyncHandler(async (req, res) => {
+    res.status(200).json();
+}));
+
+
+router.post('/verifyAccessToken', verifyAccessTokenMiddleware, asyncHandler(async (req, res) => {
+    console.log('Access token verified');
+    res.status(200).json({isAuthenticated: true});
+}));
+
+
+module.exports = router;
