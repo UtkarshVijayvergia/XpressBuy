0fe2bcd60f09c73e81d6d657722fa93b65df3a10
Utkarsh Vijayvergia <utkarsh.v1901@gmail.com>
2024-06-17 00:38:40 +0530

    refactor tokenVerificationRoutes to include new user registration endpoint

--- backend/routes/tokenVerificationRoutes.js
+++ backend/routes/tokenVerificationRoutes.js
@@ -3,8 +3,14 @@ const router = express.Router()
 const asyncHandler = require('express-async-handler')
 const { loginVerification } = require('../middlewares/userLoginVerificationMiddlewares')
 const { verifyAccessTokenMiddleware } = require('../middlewares/tokenVerificationMiddlewares')
+const { newUserRegistration } = require('../controller/userController')
+
 
 // routes for /api/v1/tokenVerification
+
+router.post('/verifyIdToken/new-user', loginVerification, newUserRegistration);
+
+
 router.post('/verifyIdToken', loginVerification, asyncHandler(async (req, res) => {
     res.status(200).json();
 }));
